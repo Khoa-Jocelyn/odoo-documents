8a8192a6b7807991647f3e2187cf000bfd5f007e
Khoa-Jocelyn <dinhvankhoa14091999@gmail.com>
2022-05-04 17:09:14 +0700

    Update module new

--- my_module/viin_user_unlink_log/models/base.py
+++ my_module/viin_user_unlink_log/models/base.py
@@ -1,16 +1,6 @@
 from odoo import models, api, fields
 from odoo.tools import config
-import datetime
 import json
-
-
-
-class JSONEncoder(json.JSONEncoder):
- 
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
-            return obj.strftime("%Y-%m-%dT%H:%M:%S")
-        return super(JSONEncoder, self).default(obj)
 
 
 class Base(models.AbstractModel):
@@ -30,32 +20,41 @@ class Base(models.AbstractModel):
     def _get_track_field_unlink_ids(self):
         ir_model_recs = self.env["ir.model"].search([("track_user_unlink", "=", True), ("model", "=", self._name)])
         ir_model_fields_recs = ir_model_recs.mapped("track_field_unlink_ids")
-        field_name_list = []
-        # for field in ir_model_fields_recs:
-        #     field_name_list.append(field.name)
-        field_name_list = ir_model_fields_recs.mapped("name")
-        return field_name_list
+        field_list = []
+        for fields in ir_model_fields_recs:
+            field = {"name": fields.name, "ttype": fields.ttype}
+            field_list.append(field)
+        return field_list
+    
+    def JSONEncoder(self, object_type, vals):
+        if object_type in ["many2many", "one2many", "many2one"]:
+            vals = [r.id for r in vals]
+        elif object_type in ["date", "datetime"]:
+            vals = fields.Datetime.to_string(vals)
+        elif object_type in ["binary"]:
+            vals = str(vals)
+        return vals
 
     def _prepare_unlink_log_vals(self):
         user_unlink_log_val_list = []
-        field_name_list = self._get_track_field_unlink_ids()
-        print(field_name_list)
+        field_list = self._get_track_field_unlink_ids()
         company_id = self.company_id.id if hasattr(self, 'company_id') and self.company_id else self.env.company.id
         for rec in self:
             record_infor_json = None
-            if field_name_list:
-                record_infor = {}
-                for field_name in field_name_list:
-                    record_infor[field_name] = getattr(rec, field_name)
-                # record_infor = rec.read(field_name_list)
-                # record_infor_json = json.dumps(record_infor, indent=1, cls=JSONEncoder)
-                record_infor_json = record_infor
             model_record = self.env['ir.model'].sudo().search([('model', '=', rec._name)], limit=1)
+            if field_list:
+                record_infor = {}
+                for field in field_list:
+                    record_infor[field["name"]] = getattr(rec, field["name"])
+                    record_infor[field["name"]] = self.JSONEncoder(field["ttype"], record_infor[field["name"]])
+                record_infor_json = json.dumps(record_infor, indent=1)
+            else:
+                record_infor_json = json.dumps({}, indent=1)
             user_unlink_log_val_list.append({"user_id": self.env.user.id,
                                              "model_id": model_record.id,
                                              "res_id": rec.id,
                                              "datetime": fields.Datetime.now(),
-                                             "res_name": rec.name,
+                                             "res_name": rec.display_name,
                                              "company_id": company_id,
                                              "record_infor": record_infor_json})
         return user_unlink_log_val_list
--- my_module/viin_user_unlink_log/models/user_unlink_log.py
+++ my_module/viin_user_unlink_log/models/user_unlink_log.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class UserUnlinkLogs(models.Model):
@@ -12,5 +12,5 @@ class UserUnlinkLogs(models.Model):
     datetime = fields.Datetime(string='Unlink Datetime', required=True, readonly=True, index=True)
     user_id = fields.Many2one('res.users', string='Unlink By User', required=True, readonly=True,index=True)
     company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True, index=True)
-    record_infor = fields.Char(string="Document  Information Unlinked", required=True, readonly=True)
+    record_infor = fields.Text(string="Document  Information Unlinked", required=True, readonly=True)
 
